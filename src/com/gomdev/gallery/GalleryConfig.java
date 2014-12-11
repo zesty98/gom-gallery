3083d4ac5cdeddaa753aa51d8a5239de44f6e757
gomdev <gomdev@gomdevui-MacBook-Pro.local>
2014-12-11 13:35:26 +0900

    merge with eclipse project

--- src/com/gomdev/gallery/GalleryConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gomdev.gallery;
-
-public class GalleryConfig {
-    static final boolean DEBUG = false;
-    static final String TAG = "gomdev";
-    
-    public static boolean sUseGLES = false;
-}
