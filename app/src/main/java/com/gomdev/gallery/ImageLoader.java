0a2063c55c5f7750aeb49859191a6800a73a0064
gomdev <gomdev@naver.com>
2015-05-11 18:41:00 +0900

    remove FileLockInterruptionException, ClosedByInterruptException

--- app/src/main/java/com/gomdev/gallery/ImageLoader.java
+++ app/src/main/java/com/gomdev/gallery/ImageLoader.java
@@ -16,6 +16,7 @@ import com.gomdev.gallery.GalleryConfig.SortBy;
 import com.gomdev.gallery.GalleryTexture.TextureState;
 
 import java.io.FileDescriptor;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -315,25 +316,6 @@ public class ImageLoader {
         }
     }
 
-    Bitmap getThumbnail(ImageInfo imageInfo,
-                        boolean forcePortrait) {
-        long imageID = imageInfo.getImageID();
-
-        Bitmap bitmap = MediaStore.Images.Thumbnails.getThumbnail(
-                mContext.getContentResolver(), imageID,
-                MediaStore.Images.Thumbnails.MINI_KIND, null);
-
-        if (bitmap == null) {
-            bitmap = decodeSampledBitmapFromFile(imageInfo, 512, 512, mImageCache);
-        }
-
-        if (forcePortrait == true) {
-            bitmap = rotate(bitmap, imageInfo.getOrientation());
-        }
-
-        return bitmap;
-    }
-
     Bitmap getBitmap(ImageInfo imageInfo, int requestWidth,
                      int requestHeight, boolean forcePortrait) {
         int orientation = imageInfo.getOrientation();
@@ -494,8 +476,18 @@ public class ImageLoader {
             if (mNeedThumbnail == true) {
 
                 bitmap = mImageCache.getBitmapFromDiskCache(imageInfo);
-                if (bitmap == null) {
-                    bitmap = mImageLoader.getThumbnail(imageInfo, true);
+                if (bitmap == null && isCancelled() == false) {
+                    long imageID = imageInfo.getImageID();
+                    synchronized (this) {
+                        bitmap = MediaStore.Images.Thumbnails.getThumbnail(
+                                mContext.getContentResolver(), imageID,
+                                MediaStore.Images.Thumbnails.MINI_KIND, null);
+                    }
+                    if (bitmap == null) {
+                        bitmap = decodeSampledBitmapFromFile(imageInfo, 512, 512, mImageCache);
+                    }
+
+                    bitmap = rotate(bitmap, imageInfo.getOrientation());
                 } else {
                     if (DEBUG) {
                         Log.d(TAG, "Disk cache hit " + imageInfo.getImagePath());
